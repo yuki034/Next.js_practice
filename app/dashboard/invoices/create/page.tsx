6a6a5c6e69facf0e22f412f014f4743223b669c8
Yuki <yutoki0304@gmail.com>
2025-06-10 18:54:22 +0900

    アドミン追加しました

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -2,7 +2,6 @@ import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
 import { Metadata } from 'next';
-import { create } from 'domain';
 
 export default async function Page() {
   const customers = await fetchCustomers();
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,20 +1,25 @@
+import { Suspense } from 'react';
 import { Metadata } from 'next';
 import AcmeLogo from '@/app/ui/acme-logo';
 import LoginForm from '@/app/ui/login-form';
 
 export default function LoginPage() {
-    return ( <main className="flex items-center justify-center md:h-screen">
-        <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
-          <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
-            <div className="w-32 text-white md:w-36">
-              <AcmeLogo />
-            </div>
+  return (
+    <main className="flex items-center justify-center md:h-screen">
+      <div className="relative mx-auto flex w-full max-w-[400px] flex-col space-y-2.5 p-4 md:-mt-32">
+        <div className="flex h-20 w-full items-end rounded-lg bg-blue-500 p-3 md:h-36">
+          <div className="w-32 text-white md:w-36">
+            <AcmeLogo />
           </div>
-          <LoginForm />
         </div>
-      </main>
-    );
-  }
-  export const metadata: Metadata = {
-    title: 'login',
-  };
+        <Suspense fallback={<div>Loading...</div>}>
+          <LoginForm />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
+
+export const metadata: Metadata = {
+  title: 'Login',
+};
