eb7a2a1618908d5c61cef3c99e20a11efb6f5468
Yuki <yutoki0304@gmail.com>
2025-06-11 15:41:55 +0900

    resultをコンソールに出力するようにしました

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -109,7 +109,7 @@ export async function GET() {
       seedInvoices(),
       seedRevenue(),
     ]);
-
+    console.log(result);
     return Response.json({ message: 'Database seeded successfully' });
   } catch (error) {
     return Response.json({ error }, { status: 500 });
