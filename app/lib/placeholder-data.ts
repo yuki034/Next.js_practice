1815dfcddf51c77c5d176e1cd5aee038f7065a93
Yuki <yutoki0304@gmail.com>
2025-06-13 15:51:12 +0900

    アドミンのパスワード変更しました

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -12,7 +12,7 @@ const users = [
     id: '410544b2-4001-4271-9855-fec4b6a6442b',
     name: 'Admin',
     email: 'admin@nextmail.com',
-    password: '123456',
+    password: '234567',
     is_admin: true,
   },
 ];
