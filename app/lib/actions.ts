c6396de2801c6346d5a8b1f2177adbc170a58353
Yuki <yutoki0304@gmail.com>
2025-06-19 15:24:03 +0900

    actions.tsの3行目のany削除しました

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { any, z } from 'zod';
+import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
