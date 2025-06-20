560f4e07c30a495f65780ee57bc2b8fe22e155a7
Yuki <yutoki0304@gmail.com>
2025-06-20 17:36:39 +0900

    使ってないUsers/pageのsession削除しました

--- app/dashboard/Users/page.tsx
+++ app/dashboard/Users/page.tsx
@@ -1,9 +1,4 @@
 import { Metadata } from 'next';
-import { auth } from '@/auth';
-
-export default async function Page() {
-    const session = await auth();//現在のユーザーのセッション情報を取得
-}
 
 export const metadata:Metadata = {
     title: 'customer',
