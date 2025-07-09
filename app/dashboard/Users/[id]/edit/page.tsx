e53d4244df6486777b80f032ea03c00c12c1995a
Yuki <yutoki0304@gmail.com>
2025-07-09 16:17:42 +0900

    型エラー修正しました

--- app/dashboard/Users/[id]/edit/page.tsx
+++ app/dashboard/Users/[id]/edit/page.tsx
@@ -4,32 +4,34 @@ import EditUserForm from '@/app/ui/Users/edit-form';
 import { fetchUsers } from '@/app/lib/data';
 import { Metadata } from 'next';
 
-export default async function Page({ params }: { params: { id: string } }) {
-    const users = await fetchUsers();
-    if (typeof params.id !== 'string') {
-      // エラー処理
-      return <div>不正なIDです</div>;
-    }
-    const user = users.find(u => u.id === String(params.id));
-    if (!user) {
-        return <div>ユーザーが見つかりません</div>;
-      }
+type Props = {
+  params: {
+    id: string;
+  };
+};
 
-return (
+export default async function Page({ params }: Props) {
+  const users = await fetchUsers();
+  const user = users.find((u) => u.id === params.id);
+  if (!user) return <div>ユーザーが見つかりません</div>;
+
+  return (
     <main>
       <Breadcrumbs
-      breadcrumbs={[
-  { label: 'Users', href: '/dashboard/users' },
-  {
-    label: 'Edit Users',
-    href: `/dashboard/Users/edit`,
-    active: true,
-  },  ]}
-  />
-  <EditUserForm user={user} />
-</main>
-);
+        breadcrumbs={[
+          { label: 'Users', href: '/dashboard/users' },
+          {
+            label: 'Edit Users',
+            href: `/dashboard/Users/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <EditUserForm user={user} />
+    </main>
+  );
 }
+
 export const metadata: Metadata = {
 title: 'edit',
 };
